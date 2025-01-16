72f0b96bb8f735fb6b845834fb0c6e6f9ba54761
Luka Budík <sheffikthegod@gmail.com>
2025-01-16 15:00:46 +0100

    feat: new dates

--- data/course-dates.ts
+++ data/course-dates.ts
@@ -1,44 +1,37 @@
 export const courseDates = [
   {
-    date: '2025-01-17',
-    type: 'Prezenční',
-    location: 'Staroměstké náměstí / Můstek, Praha',
-    cityLocative: 'Praze',
-    deadline: '2024-12-18'
-  },
-  {
-    date: '2025-02-11',
-    type: 'Prezenční',
-    location: 'JIC, Brno',
-    cityLocative: 'Brně',
-    deadline: '2025-01-12'
-  },
-  {
     date: '2025-02-28',
     type: 'Prezenční',
-    location: 'Staroměstké náměstí / Můstek, Praha',
+    location: 'Praha',
     cityLocative: 'Praze',
-    deadline: '2025-01-29'
+    deadline: '2025-01-28'
   },
   {
     date: '2025-03-08',
     type: 'Prezenční',
-    location: 'Inovační středisko Inion, Karlovy Vary',
+    location: 'Karlovy Vary',
     cityLocative: 'Karlových Varech',
     deadline: '2025-02-08'
   },
   {
+    date: '2025-03-18',
+    type: 'Prezenční',
+    location: 'Praha',
+    cityLocative: 'Praze',
+    deadline: '2025-02-18'
+  },
+  {
     date: '2025-03-21',
     type: 'Prezenční',
-    location: 'V Jámě, Praha',
+    location: 'Praha',
     cityLocative: 'Praze',
     deadline: '2025-02-21'
   },
   {
-    date: '2025-03-24',
+    date: '2025-03-25',
     type: 'Prezenční',
-    location: 'JIC, Brno',
+    location: 'Brno',
     cityLocative: 'Brně',
-    deadline: '2025-02-24'
+    deadline: '2025-02-25'
   }
 ];
